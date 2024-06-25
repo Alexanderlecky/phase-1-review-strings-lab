2915585ee691e2c3c720b7fed8ad3bc2e4c80cbc
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-06-25 13:46:38 +0300

    Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+// Define the currentUser variable
+const currentUser = "Grace Hopper";
+
+// Define the welcomeMessage using template literals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage, transforming welcomeMessage to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define the shortGreeting, using the first character of currentUser
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+
+
+
+
+
